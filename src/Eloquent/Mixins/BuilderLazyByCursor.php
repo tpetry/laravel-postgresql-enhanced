c2d4c2df05bd08f67b50591bdaa106efe8342e3c
tpetry <github@tpetry.me>
2024-09-23 17:39:22 +0200

    fix: add lazy loading to lazyByCursor for eloquent queries (resolves #95)

--- src/Eloquent/Mixins/BuilderLazyByCursor.php
+++ src/Eloquent/Mixins/BuilderLazyByCursor.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tpetry\PostgresqlEnhanced\Eloquent\Mixins;
 
 use Closure;
+use Generator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\LazyCollection;
 
@@ -15,8 +16,17 @@ class BuilderLazyByCursor
     {
         return function (int $chunkSize = 1000): LazyCollection {
             /* @var \Illuminate\Database\Eloquent\Builder $this */
-            return $this->applyScopes()->getQuery()->lazyByCursor($chunkSize)->map(function (object $record): Model {
-                return $this->newModelInstance()->newFromBuilder((array) $record);
+            return new LazyCollection(function () use ($chunkSize): Generator {
+                foreach ($this->applyScopes()->getQuery()->lazyByCursor($chunkSize)->chunk($chunkSize) as $items) {
+                    $models = $this->getModel()->hydrate($items->all())->all();
+                    if (count($models) > 0) {
+                        $models = $this->eagerLoadRelations($models);
+                    }
+
+                    foreach ($models as $model) {
+                        yield $model;
+                    }
+                }
             });
         };
     }
