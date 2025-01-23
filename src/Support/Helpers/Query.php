18bc9f4191f04768b54cc593a255d7f7bb53edd4
tpetry <github@tpetry.me>
2025-01-23 11:56:36 +0100

    chore: use backported toRawSql functionality

--- src/Support/Helpers/Query.php
+++ src/Support/Helpers/Query.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Tpetry\PostgresqlEnhanced\Support\Helpers;
 
-use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Query\Builder as QueryBuilder;
-use RuntimeException;
 
 class Query
 {
@@ -20,29 +18,8 @@ class Query
             return $query;
         }
 
-        /** @var Connection $connection */
-        $connection = $query->getConnection();
-
-        $bindings = $connection->prepareBindings($query->getBindings());
-        $sql = preg_replace_callback('/(?<!\?)\?(?!\?)/', function () use (&$bindings, $connection) {
-            if (0 === \count($bindings)) {
-                throw new RuntimeException('Number of bindings does not match the number of placeholders');
-            }
-
-            $value = array_shift($bindings);
-
-            return (string) match (true) {
-                null === $value => 'null',
-                \is_bool($value) => $value ? 'true' : 'false',
-                is_numeric($value) => $value,
-                default => $connection->getPdo()->quote((string) $value),
-            };
-        }, $query->toSql());
-
-        if (\count($bindings) > 0) {
-            throw new RuntimeException('Number of bindings does not match the number of placeholders');
-        }
-
-        return $sql;
+        return $query->getGrammar()->substituteBindingsIntoRawSql(
+            $query->toSql(), $query->getConnection()->prepareBindings($query->getBindings())
+        );
     }
 }
