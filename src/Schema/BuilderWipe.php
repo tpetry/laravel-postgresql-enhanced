1aabe7e37e9a16fb38457df06dc079e8419bbae1
tpetry <github@tpetry.me>
2025-03-05 21:04:30 +0100

    fix: improved handling of hypertables/views not to drop (resolves #104)

--- src/Schema/BuilderWipe.php
+++ src/Schema/BuilderWipe.php
@@ -4,46 +4,45 @@ declare(strict_types=1);
 
 namespace Tpetry\PostgresqlEnhanced\Schema;
 
+use Illuminate\Support\Collection;
+
 trait BuilderWipe
 {
     public function dropAllContinuousAggregates(): void
     {
-        $continuousAggregates = rescue(
-            callback: fn () => $this->getConnection()->table('timescaledb_information.continuous_aggregates')->select(['view_schema', 'view_name'])->whereIn('view_schema', $this->getActiveSchemas())->get(),
-            rescue: [],
-            report: false,
+        $continuousAggregates = $this->findRelations(
+            rescue(
+                callback: fn () => $this->getConnection()->table('timescaledb_information.continuous_aggregates')->select(['view_schema as schema', 'view_name as name'])->whereIn('view_schema', $this->getActiveSchemas())->get(),
+                rescue: [],
+                report: false,
+            )
         );
         foreach ($continuousAggregates as $continuousAggregate) {
-            $name = "{$continuousAggregate->view_schema}.{$continuousAggregate->view_name}";
-            if (!\in_array($name, $this->connection->getConfig('dont_drop') ?? [])) {
-                $this->connection->statement("drop materialized view if exists {$this->grammar->wrap($name)} cascade");
-            }
+            $this->connection->statement("drop materialized view if exists {$this->grammar->wrap($continuousAggregate['schema_qualified_name'])} cascade");
         }
     }
 
     public function dropAllHypertables(): void
     {
-        $hypertables = rescue(
-            callback: fn () => $this->getConnection()->table('timescaledb_information.hypertables')->select(['hypertable_schema', 'hypertable_name'])->whereIn('hypertable_schema', $this->getActiveSchemas())->get(),
-            rescue: [],
-            report: false,
+        $hypertables = $this->findRelations(
+            rescue(
+                callback: fn () => $this->getConnection()->table('timescaledb_information.hypertables')->select(['hypertable_schema as schema', 'hypertable_name as name'])->whereIn('hypertable_schema', $this->getActiveSchemas())->get(),
+                rescue: [],
+                report: false,
+            )
         );
         foreach ($hypertables as $hypertable) {
-            $name = "{$hypertable->hypertable_schema}.{$hypertable->hypertable_name}";
-            if (!\in_array($name, $this->connection->getConfig('dont_drop') ?? [])) {
-                $this->connection->statement("drop table if exists {$this->grammar->wrap($name)} cascade");
-            }
+            $this->connection->statement("drop table if exists {$this->grammar->wrap($hypertable['schema_qualified_name'])} cascade");
         }
     }
 
     public function dropAllMaterializedViews(): void
     {
-        $materializedViews = $this->getConnection()->table('pg_matviews')->select(['schemaname', 'matviewname'])->whereIn('schemaname', $this->getActiveSchemas())->get();
+        $materializedViews = $this->findRelations(
+            $this->getConnection()->table('pg_matviews')->select(['schemaname as schema', 'matviewname as name'])->whereIn('schemaname', $this->getActiveSchemas())->get()
+        );
         foreach ($materializedViews as $materializedView) {
-            $name = "{$materializedView->schemaname}.{$materializedView->matviewname}";
-            if (!\in_array($name, $this->connection->getConfig('dont_drop') ?? [])) {
-                $this->connection->statement("drop materialized view if exists {$this->grammar->wrap($name)} cascade");
-            }
+            $this->connection->statement("drop materialized view if exists {$this->grammar->wrap($materializedView['schema_qualified_name'])} cascade");
         }
     }
 
@@ -57,7 +56,35 @@ trait BuilderWipe
     {
         $this->dropAllContinuousAggregates();
         $this->dropAllMaterializedViews();
-        parent::dropAllViews();
+
+        $views = $this->findRelations($this->connection->table('pg_views')->select(['schemaname as schema', 'viewname as name'])->whereIn('schemaname', $this->getActiveSchemas())->get(), [
+            // PostGIS
+            'geometry_columns',
+            'geography_columns',
+            // pg_buffercache
+            'pg_buffercache',
+            // pgRouting
+            'raster_columns',
+            'raster_overviews',
+        ]);
+        if (filled($views)) {
+            $this->connection->statement("drop view {$this->grammar->columnize(array_column($views, 'schema_qualified_name'))} cascade");
+        }
+    }
+
+    /**
+     * @return array<array{schema_qualified_name: string}>
+     */
+    private function findRelations(Collection $relations, array $exclude = []): array
+    {
+        $excludedRelations = array_merge($this->connection->getConfig('dont_drop') ?? [], $exclude);
+
+        return array_filter(
+            array: $this->connection->getPostProcessor()->processTables($relations->all()),
+            callback: function ($relation) use ($excludedRelations) {
+                return blank(array_intersect([$relation['name'], $relation['schema_qualified_name']], $excludedRelations));
+            }
+        );
     }
 
     /**
